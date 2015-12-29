70149e7f99aeba78c034ada4e89a8c92809cb7aa
J.Fukaya <fukajun.shark@gmail.com>
2015-12-29 11:16:09 +0900

    Implement ReWebSocket

--- source/main.js
+++ source/main.js
@@ -5,9 +5,11 @@
 import menubar from 'menubar';
 import { app, ipcMain } from 'electron';
 import notifier from 'node-notifier';
+import { EventEmitter } from 'events';
 
 const request = require('request');
 const mb = menubar({ icon: __dirname + '/images/read.png' });
+var WebSocket = require('ws');
 
 const switchIconUnread = ()=> {
   mb.tray.setImage(__dirname + '/images/unread.png')
@@ -15,33 +17,48 @@ const switchIconUnread = ()=> {
 const switchIconRead = ()=> {
   mb.tray.setImage(__dirname + '/images/read.png')
 }
+
+class ReWebSocket extends EventEmitter {
+  constructor(url) {
+    super()
+    this.url = url
+    this.websocket()
+  }
+  send(...args) {
+    this.websocket().send(...args)
+  }
+  websocket() {
+    if(!this.ws) {
+      this.ws = new WebSocket(this.url);
+      this.ws.on('message', (msg)=> {
+        this.emit('message', msg)
+      })
+      this.ws.on('close', (...args)=> {
+        this.emit('close', ...args)
+        setTimeout(()=>{this.websocket()}, 1000);
+      });
+      this.ws.on('open', (...args)=> {
+        this.emit('open', ...args)
+      })
+    }
+    return this.ws
+  }
+}
+
 mb.on('ready', function ready () {
 
-  //using the ws library
-  var WebSocket = require('ws');
-  var w = new WebSocket("wss://api2.bitfinex.com:3000/ws");
+  let w = new ReWebSocket("wss://api2.bitfinex.com:3000/ws")
   w.on('open', ()=> {
     w.send(JSON.stringify({
         "event": "subscribe",
         "channel": "trades",
         "pair": "BTCUSD"
     }))
-    //w.send(JSON.stringify({
-         //"event":"subscribe",
-         //"channel":"book",
-         //"pair":"BTCUSD",
-         //"prec":"P0" // R0 = raw
-    //}))
-    //w.send(JSON.stringify({
-        //"event": "subscribe",
-        //"channel": "ticker",
-        //"pair": "BTCUSD"
-    //}))
   })
   ipcMain.on('fetch_request', function(event, arg) {
-    w.onmessage = function(msg) {
+    w.on('message', (msg)=> {
       event.sender.send('fetch_response', msg);
-    };
+    });
   });
 
   ipcMain.on('notify', (event, title, message)=> {
